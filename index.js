f3964be4dd895c142c4008bcc41440cf72401476
Bidhu1024 <bidhu1024av@gmail.com>
2023-01-22 11:19:19 +0530

    18.06

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-import express from 'express'
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./Routes/auth.js";
+import userRoute from "./Routes/auth.js";
+import roomsRoute from "./Routes/auth.js";
+import hotelRoute from "./Routes/auth.js";
+const app = express();
+dotenv.config();
+mongoose.set("strictQuery", false);
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("connected to mongo");
+  } catch (error) {
+    throw error;
+  }
+};
 
-const app = express()
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected!");
+});
 
-app.listen(8800,()=>{
-    console.log('backend connected.');
-})
+//middlewares
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", authRoute);
+
+app.use("/api/rooms", authRoute);
+
+app.use("/api/hotel", authRoute);
+
+app.listen(8800, () => {
+  connect();
+  console.log("backend connected.");
+});
